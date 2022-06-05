516d4a7fa7c899e4741dee7397b3b7f1965361c8
olimajs <jsapps@outlook.es>
2022-06-05 20:03:21 -0300

    udpate files of the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Quote from "../src/componentes/Quote";
+import Spinner from "./componentes/Spinner";
+
+const initialQuote = {
+  text: 'Quote',
+  author: 'Author:)'
+}
+
 
 function App() {
+  const [quote, setQuote] = useState(initialQuote);
+
+  const [loading, setLoading] = useState(false);
+
+
+  const updateQuote = async () => {
+    setLoading(true); /* cuando se actualiza se pone cargando */
+
+    const url = "https://www.breakingbadapi.com/api/quote/random";
+    /*es un proceso asincrono x eso hay que usar un await, pero el effect no acepta entonces se crea la funcion de arriba*/
+    const res = await fetch(url);
+    /*p/extraer la data se pone:*/
+    const [newQuote] = await res.json();
+    
+    /* desestructuramos para simplificar lo de setquote*/
+    const { quote: text, author } = newQuote;
+
+    /*almacenamos el nuevo sestado de quote mediante setquote*/
+    setQuote( {
+      text: text,
+      author,
+    })
+    setLoading(false); /*cuando termina figura el quote*/
+  }
+
+  useEffect(() => {
+    updateQuote();  
+  }, []); /*para poder llamar una funcion, solo la 2era vez que se renderiza un comp se usa useeffect(mas adelante repasar eso)*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/7/77/Breaking_Bad_logo.svg"
+        alt="logo"
+        />
+      <button onClick={() => updateQuote()}> Get Another </button>
+
+    { loading ? <Spinner /> :  <Quote quote = {quote} /> }
+
+  
     </div>
   );
 }
---
+++ src/componentes/Spinner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Spinner.css';
+const Spinner = () => {
+    return( 
+        <div class="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>
+    );
+};
+
+export default Spinner;
